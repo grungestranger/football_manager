548067fde83acf427402012bce876ff8c7238df2
grungestranger <k.shevchenko@smartcodes.ru>
2017-12-14 17:45:12 +0300

    master 2017-12-14

--- public/js/team.js
+++ public/js/team.js
@@ -41,17 +41,24 @@ $(document).ready(function(){
         axis: 'y',
         opacity: 0.6,
 		stop: function(e, ui) {
-			var id1 = ui.helper.data('id');
+			var id = [];
+			id[0] = ui.helper.data('id');
 			ui.helper.remove();
-			var row1 = $('#players > tbody > tr[data-id="' + id1 + '"]');
-			var html1 = row1.html();
 			$('#players > tbody > tr').each(function(){
 				if (ui.offset.top - $(this).offset().top < $(this).height() / 2) {
-					var id2 = $(this).data('id');
-					var row2 = $(this);
-					var html2 = row2.html();
-					row1.html(html2)/*.attr('data-id', id2)*/;
-					row2.html(html1)/*.attr('data-id', id1)*/;
+					id[1] = $(this).data('id');
+					if (id[0] != id[1]) {
+						var row = [], html = [];
+						row[0] = $('#players > tbody > tr[data-id="' + id[0] + '"]');
+						row[1] = $(this);
+						for (var i = 0; i <= 1; i++) {
+							html[i] = row[i].html();
+						}
+						for (var i = 0; i <= 1; i++) {
+							var j = Math.abs(i - 1);
+							row[i].html(html[j]).attr('data-id', id[j]).data('id', id[j]);
+						}
+					}
 					return false;
 				}
 			});
