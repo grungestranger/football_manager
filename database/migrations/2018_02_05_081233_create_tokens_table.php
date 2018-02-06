44390fbf8c1503c5e4ae24288650f0432073b296
grungestranger <grungestranger@yandex.ru>
2018-02-06 17:34:15 +0300

    master 2018-02-06

---
+++ database/migrations/2018_02_05_081233_create_tokens_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->char('token', 25)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tokens');
+    }
+}
