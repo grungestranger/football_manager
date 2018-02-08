1b94ad69e35f87c23d70f293015bd8347c53444a
grungestranger <grungestranger@yandex.ru>
2018-02-08 15:51:31 +0300

    master 2018-02-08

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,7 +29,6 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \App\Http\Middleware\UserHandler::class,
         ],
 
         'api' => [
---
+++ database/migrations/2018_02_08_115630_create_challenges_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->integer('user_from');
+            $table->integer('user_to');
+            $table->timestamp('created_at');
+
+            $table->unique(['user_from', 'user_to']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('challenges');
+    }
+}
